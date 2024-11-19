f7e66676caaa81e24942241fb878874dbab20d67
Antonio Carlos S P <antoniocsprudencio@gmail.com>
2024-11-18 23:04:19 -0300

    feat - criação modelo básico para outras models

---
+++ src/main/java/com/pebatech/br/cadastro_api/domain/model/ModeloGenerico.java
@@ -0,0 +1,44 @@
+package com.pebatech.br.cadastro_api.domain.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@MappedSuperclass
+public class ModeloGenerico implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Version
+    Long versao;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @GeneratedValue(generator = "UUID")
+    @Column(unique = true, updatable = false, nullable = false)
+    private UUID uuid;
+
+    @Column(columnDefinition = "boolean default true")
+    private Boolean ativo = true;
+
+    @CreationTimestamp
+    private LocalDateTime dataCriacao;
+
+    @UpdateTimestamp
+    private LocalDateTime dataAtualizacao;
+}
